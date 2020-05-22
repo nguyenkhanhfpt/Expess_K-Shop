51612d865fc7fd163095d87d3f4893e53af3816f
Nguyễn Khánh <khanh26122000@gmail.com>
2020-05-22 22:21:22 +0700

    install package and set up project

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+// package required
+const express = require('express');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+const port = 3000;
+
+// set view engines
+app.set('view engine', 'pug');
+app.set('views', '/views');
+
+// use middleware
+app.use(cookieParser());
+
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+    res.send('Home');
+});
+
+app.listen(port, () => console.log(`Server is running in port ${port}`));
